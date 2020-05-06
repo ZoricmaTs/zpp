af435ab79342c151e1607f64f0403d9258075bf5
Fessan <fessan@ya.ru>
2020-05-06 23:28:14 +0700

    add 50% home

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,119 @@
 <template>
-  <section class="">
+  <section>
     <header class="container w-1200" id="header">
-      <div class="wrapper_header ml-5">
-        <img src="@/assets/img/zeleny.png" alt="logo" class="logo mt-5" />
-        <h1>
-          ФИТНЕС ОНЛАЙН <br />
-          С КОЛЛЕКТВНОЙ ПОДДЕРЖКОЙ
-        </h1>
+      <div class="row">
+        <div class="wrapper_header ml-5 col-lg-8">
+          <img
+            src="@/assets/img/zeleny.png"
+            alt="logo"
+            class="logo mt-5 img-fluid"
+          />
+          <h1 class="header_h1 mt-3 mb-3">
+            ФИТНЕС ОНЛАЙН
+            <br />С КОЛЛЕКТВНОЙ ПОДДЕРЖКОЙ
+          </h1>
+          <p class="header_p">
+            Художественный ритуал многопланово образует эпитет. Интерпретация,
+            по определению, изящно дает фактографический горизонт ожидания.
+            многопланово аккумулирует онтогенез.
+          </p>
+          <div class>
+            <button class="btn mr-4 header__btn mt-3 pt-2 pb-2">ВОЙТИ</button>
+            <button class="btn ml-5 header__btn mt-3 pt-2 pb-2">
+              ЗАРЕГИСТРИРОВАТЬСЯ
+            </button>
+          </div>
+        </div>
       </div>
     </header>
+    <main>
+      <section class="main__advantages w-1200 container">
+        <div class="row">
+          <img
+            src="@/assets/img/levo.png"
+            alt="levo"
+            class="col-lg-2 main__img_left img-fluid"
+          />
+          <div class="main__advant_div col-10">
+            <h2 class="mt-5 mb-5">
+              ПРЕИМУЩЕСТВА ФИТНЕСА
+              <br />
+              С КОЛЛЕКТИВНОЙ ПОДДЕРЖКОЙ
+            </h2>
+            <div class="container mt-3">
+              <div class="row mt-5">
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 1</p>
+                </div>
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 2</p>
+                </div>
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 3</p>
+                </div>
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 4</p>
+                </div>
+              </div>
+              <div class="row mt-5">
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 1</p>
+                </div>
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 2</p>
+                </div>
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 3</p>
+                </div>
+                <div class="advant col-lg-2 ml-5">
+                  <img
+                    src="@/assets/img/iron.png"
+                    alt="iron"
+                    class="img-fluid mx-auto d-block"
+                  />
+                  <p class="text-center mt-4">Преимущество 4</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </section>
 </template>
 
