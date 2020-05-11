5c802643ddf6025ab269ac4ec2724fce2bb9cc9e
Fessan <fessan@ya.ru>
2020-05-12 01:33:43 +0700

    add slider

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './modules/auth'
+import reviews from './modules/reviews'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    reviews
   }
 })
